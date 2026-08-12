f9a5019ff3536a5704e507de5e11fe4e1d1c525a
Mariana Fernandes <marizin28@gmail.com>
2026-08-12 16:26:48 -0300

    Adicionar arquivos de projeto.

---
+++ TelaInicialCrud/Areas/Identity/Data/Projeto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelaInicialCrud.Areas.Identity.Data
+{
+    public class Projeto
+    {
+        [Key]
+        public int IdProjeto { get; set; }
+        public string NomeProjeto { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public int QuantPessoas { get; set; }
+
+    }
+}
---
+++ TelaInicialCrud/Areas/Identity/Data/TelaInicialCrudContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Identity.Client;
+using TelaInicialCrud.Areas.Identity.Data;
+
+namespace TelaInicialCrud.Areas.Identity.Data;
+
+public class TelaInicialCrudContext : IdentityDbContext<Usuario>
+{
+    public TelaInicialCrudContext(DbContextOptions<TelaInicialCrudContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Projeto> Projeto { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.ApplyConfiguration(new ConfigUser());
+    }
+
+    public class ConfigUser : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.Property(x => x.Nome).HasMaxLength(150);
+            builder.Property(x => x.Sobrenome).HasMaxLength(150);
+        }
+    }
+}
